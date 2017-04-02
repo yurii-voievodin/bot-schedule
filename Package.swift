e1728ddb7beeb60deac9dba6d508693387d6defb
Yura Voevodin <voevodin.yura@icloud.com>
2017-04-02 11:14:48 +0300

    Turned back Kanna library

--- Package.swift
+++ Package.swift
@@ -4,10 +4,7 @@ let package = Package(
     name: "SumDUBot",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-
-        // TODO: Turn on when issue with Kanna will be fixed https://github.com/tid-kijyun/Kanna/issues/142
-//        .Package(url: "https://github.com/tid-kijyun/Kanna.git", majorVersion: 2),
-
+        .Package(url: "https://github.com/tid-kijyun/Kanna.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
     ],
--- Sources/App/Commands/ImportCommand.swift
+++ Sources/App/Commands/ImportCommand.swift
@@ -9,7 +9,7 @@
 import Vapor
 import Console
 import HTTP
-//import Kanna
+import Kanna
 import Foundation
 import Rainbow
 
@@ -46,19 +46,17 @@ final class ImportCommand: Command {
     // MARK: - Lifecycle
 
     public func run(arguments: [String]) throws {
-//        guard let firstArgument = arguments.first else { throw ImportError.missingArguments }
-//        guard let argument = Argument(rawValue: firstArgument) else { throw ImportError.unknownArgument }
-//
-//        switch argument {
-//        case .objects:
-//            try importObjects()
-//        }
+        guard let firstArgument = arguments.first else { throw ImportError.missingArguments }
+        guard let argument = Argument(rawValue: firstArgument) else { throw ImportError.unknownArgument }
+
+        switch argument {
+        case .objects:
+            try importObjects()
+        }
     }
 }
 
 // MARK: - Import objetcs
-
-// TODO: Turn on when issue with Kanna will be fixed https://github.com/tid-kijyun/Kanna/issues/142
 
 extension ImportCommand {
 
@@ -68,37 +66,37 @@ extension ImportCommand {
     ///   - selector: selector for "select" HTML attribute
     ///   - document: document to fetch data
     /// - Returns: array of data - name: id
-//    fileprivate func extractData(with selector: String, from document: HTMLDocument) -> Dictionary<String, Any> {
-//        var data: Dictionary = [String: Any]()
-//
-//        for option in document.css(selector) {
-//            for value in option.css("option") {
-//                guard let name = value.content, let id = value["value"] else { continue }
-//                data[name] = id
-//            }
-//        }
-//        return data
-//    }
+    fileprivate func extractData(with selector: String, from document: HTMLDocument) -> Dictionary<String, Any> {
+        var data: Dictionary = [String: Any]()
 
-//    fileprivate func importObjects() throws {
-//        // Request to server with schedule
-//        let response = try drop.client.get("http://schedule.sumdu.edu.ua")
-//        guard let bodyBytes = response.body.bytes else { return }
-//        guard let htmlString = String(bytes: bodyBytes, encoding: .windowsCP1251) else { return }
-//        guard let document = HTML(html: htmlString, encoding: .windowsCP1251) else { return }
-//
-//        // Extract data from HTML
-//        let groups = extractData(with: "select#group", from: document)
-//        let auditoriums = extractData(with: "select#auditorium", from: document)
-//        let teachers = extractData(with: "select#teacher", from: document)
-//
-//        // Import all data to database
-//        try ObjectsImportManager.importFrom(groups, for: .group)
-//        try ObjectsImportManager.importFrom(auditoriums, for: .auditorium)
-//        try ObjectsImportManager.importFrom(teachers, for: .teacher)
-//
-//        // Success
-//        let count = try Object.all().count
-//        print("\(count) objects imported".green)
-//    }
+        for option in document.css(selector) {
+            for value in option.css("option") {
+                guard let name = value.content, let id = value["value"] else { continue }
+                data[name] = id
+            }
+        }
+        return data
+    }
+
+    fileprivate func importObjects() throws {
+        // Request to server with schedule
+        let response = try drop.client.get("http://schedule.sumdu.edu.ua")
+        guard let bodyBytes = response.body.bytes else { return }
+        guard let htmlString = String(bytes: bodyBytes, encoding: .windowsCP1251) else { return }
+        guard let document = HTML(html: htmlString, encoding: .windowsCP1251) else { return }
+
+        // Extract data from HTML
+        let groups = extractData(with: "select#group", from: document)
+        let auditoriums = extractData(with: "select#auditorium", from: document)
+        let teachers = extractData(with: "select#teacher", from: document)
+
+        // Import all data to database
+        try ObjectsImportManager.importFrom(groups, for: .group)
+        try ObjectsImportManager.importFrom(auditoriums, for: .auditorium)
+        try ObjectsImportManager.importFrom(teachers, for: .teacher)
+
+        // Success
+        let count = try Object.all().count
+        print("\(count) objects imported".green)
+    }
 }
