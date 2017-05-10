5f8d0c252d705bee6a9758e703b934c626386af0
Yura Voevodin <voevodin.yura@icloud.com>
2017-05-10 21:02:21 +0300

    Counting requests only for bot (completed).

--- Sources/App/Managers/RequestsManager.swift
+++ Sources/App/Managers/RequestsManager.swift
@@ -15,16 +15,20 @@ class RequestsManager {
     func addRequest() {
         guard let (day, month, year) = Date().calendarComponents else { return }
         
-        if var session = try? Session.query()
-            .filter("year", .equals, year)
-            .filter("month", .equals, month)
-            .filter("day", .equals, day)
-            .first() {
-            session?.requests += 1
-            try? session?.save()
-        } else {
-            var newSession = Session(day: day, month: month, year: year, requests: 1)
-            try? newSession.save()
+        do {
+            if var session = try Session.query()
+                .filter("year", .equals, year)
+                .filter("month", .equals, month)
+                .filter("day", .equals, day)
+                .first() {
+                session.requests += 1
+                try session.save()
+            } else {
+                var newSession = Session(day: day, month: month, year: year, requests: 1)
+                try newSession.save()
+            }
+        } catch  {
+            print(error.localizedDescription)
         }
     }
 }
