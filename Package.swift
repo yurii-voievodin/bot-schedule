e9676f8c0bf3e121154750031ec413f12009e652
Yura Voevodin <voevodin.yura@icloud.com>
2017-06-01 17:09:08 +0300

    Updated packages to latest versions.

--- Package.swift
+++ Package.swift
@@ -3,16 +3,15 @@ import PackageDescription
 let package = Package(
     name: "SumDUBot",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
-        .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
-        .Package(url: "https://github.com/kgn/Jobs.git", majorVersion: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 2, minor: 0)
     ],
     exclude: [
         "Config",
         "Database",
-        "Localization",
         "Public",
-        "Resources",
+        "Resources"
     ]
 )
